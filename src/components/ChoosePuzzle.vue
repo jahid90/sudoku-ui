e3f3d5b1a8145f721b514990cf0065af921be512
Jahid <s.jahid.90@gmail.com>
2019-06-23 21:39:50 +0530

    remove boilerplate code and add sample landing page content

---
+++ src/components/ChoosePuzzle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="choose-puzzle">
+        <p>Choose a puzzle</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChoosePuzzle",
+    props: {
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <ChoosePuzzle />
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ChoosePuzzle from "@/components/ChoosePuzzle.vue";
 
 export default {
     name: "home",
     components: {
-        HelloWorld
+        ChoosePuzzle
     }
 };
 </script>
